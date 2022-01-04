0b455ccb7fd7c693198f0dcff2e9b4749680304c
KoenigJulia <julia04school@gmail.com>
2022-01-04 17:32:27 +0100

    Add grpc service (not working)

---
+++ backend/src/main/java/at/htl/tennis/boundary/HelloResource.java
@@ -0,0 +1,39 @@
+package at.htl.tennis.boundary;
+
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+
+import at.htl.tennis.Greeter;
+import at.htl.tennis.HelloReply;
+import at.htl.tennis.HelloRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+    @GrpcClient
+    Greeter hello;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+
+    @GET
+    @Path("/{name}")
+    public Uni<String> hello(@PathParam("name") String name) {
+        return hello.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform(helloReply -> helloReply.getMessage());
+    }
+
+    @GET
+    @Path("/mutiny/{name}")
+    public Uni<String> helloMutiny(@PathParam("name") String name) {
+        return hello.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform(HelloReply::getMessage);
+    }
+}
---
+++ backend/src/main/java/at/htl/tennis/control/HelloService.java
@@ -0,0 +1,21 @@
+package at.htl.tennis.control;
+
+import io.grpc.stub.StreamObserver;
+import at.htl.tennis.Greeter;
+import at.htl.tennis.HelloReply;
+import at.htl.tennis.HelloRequest;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
+
+@GrpcService
+public class HelloService implements Greeter {
+
+    @Override
+    public Uni<HelloReply> sayHello(HelloRequest request) {
+        return Uni.createFrom().item(() ->
+                HelloReply.newBuilder().setMessage("Hello " + request.getName()).build()
+        );
+    }
+
+}
